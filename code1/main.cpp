f4dbd07caef90c54a2f45d0ad7c8c239c07eea59
Primemaker <infamywong@gmail.com>
2021-05-13 11:13:07 +0800

    initial commit

---
+++ code1/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+// Solution 1
+int countOne(long number) {
+//    string numStr = (number);
+    char numStr[999];
+    sprintf(numStr, "%ld", number);
+    cout << numStr << endl;
+
+    int count = 0;
+    for (int i = 0; numStr[i] != '\0'; i++) {
+        if (numStr[i] == '1') {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+
+// Solution 2
+int countOne_(long number) {
+    stringstream ss;
+    ss << number;
+
+    string numStr = ss.str();
+    cout << numStr << endl;
+
+    int count = 0;
+    for (int i = 0; i < numStr.size(); i++) {
+        if (numStr[i] == '1') {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+
+// Solution 3
+int countOne__(long number) {
+
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << countOne(n) << endl;
+    cout << countOne_(n) << endl;
+}
